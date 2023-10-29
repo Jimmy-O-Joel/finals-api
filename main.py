76d97b9727336030424a200b4c0117b158737f04
jimmy joel <jimmyjoel177@gmail.com>
2023-10-29 15:17:49 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+IMG_HEIGHT = 256
+IMG_WIDTH = 256
+
+
+app = FastAPI()
+
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+#     "https://potato-disease-classification.netlify.app"
+# ]
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+try:
+    MODEL = tf.keras.models.load_model("potatoes.h5")
+    print("Model loaded successfully.")
+except Exception as e:
+    print(f"Error loading model: {str(e)}")
+
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    image = np.resize(image, (IMG_HEIGHT, IMG_WIDTH, 3))
+    image = image.astype('float32')
+    image /= 255
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
+
